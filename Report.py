b62fb7946575524bb75622fb53c0c16492cce7b4
Carlos-Proyecto <carlos.serrano@pcm.com.ve>
2026-09-18 15:38:07 -0400

    Update Report.py

--- Report.py
+++ Report.py
@@ -5,7 +5,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Importar el handler del archivo trasladovalores.py
-from Traslado_valores import traslado_handler
+from Traslado_valores.py import traslado_handler
 
 # Cargar variables de entorno (.env)
 load_dotenv()
