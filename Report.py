bd33f7da08dd4c27eb4aca5d714df60a09fa561c
Carlos-Proyecto <carlos.serrano@pcm.com.ve>
2026-09-22 11:55:25 -0400

    Update Report.py

--- Report.py
+++ Report.py
@@ -6,10 +6,11 @@ from telegram.ext import (
     ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 )
 
-# Importar los handlers desde la carpeta reportes/
+# Importar los handlers desde la carpeta reportes/ (Orden alfabético)
 from reportes.acta_emergencia import acta_handler
 from reportes.bienes_hallados import bienes_hallados_handler
 from reportes.entrega_cadaver import entrega_cadaver_handler
+from reportes.evento_estacionamiento import evento_estacionamiento_handler
 from reportes.notificacion_deceso import deceso_handler
 from reportes.plantilla_guardia import plantilla_handler
 from reportes.traslado_valores import traslado_handler
@@ -26,6 +27,7 @@ async def start_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
         [InlineKeyboardButton("Acta de Emergencia", callback_data="iniciar_acta")],
         [InlineKeyboardButton("Bienes Hallados", callback_data="iniciar_bienes")],
         [InlineKeyboardButton("Entrega de Cadáver", callback_data="iniciar_entrega")],
+        [InlineKeyboardButton("Evento en Estacionamiento", callback_data="iniciar_evento_estac")],
         [InlineKeyboardButton("Notificación de Deceso", callback_data="iniciar_deceso")],
         [InlineKeyboardButton("Plantilla de Guardia", callback_data="iniciar_plantilla")],
         [InlineKeyboardButton("Traslado de Valores", callback_data="iniciar_traslado")],
@@ -55,6 +57,7 @@ if __name__ == '__main__':
     app.add_handler(acta_handler)
     app.add_handler(bienes_hallados_handler)
     app.add_handler(entrega_cadaver_handler)
+    app.add_handler(evento_estacionamiento_handler)
     app.add_handler(deceso_handler)
     app.add_handler(plantilla_handler)
     app.add_handler(traslado_handler)
